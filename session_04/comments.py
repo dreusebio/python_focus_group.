0d5b6a9dc243235c51d72822fe4419b81f34b654
George Eusebio Kuodza <83031200+dreusebio@users.noreply.github.com>
2023-04-11 11:37:15 -0700

    Create comments.py

---
+++ session_04/comments.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+print("Here is an examples of an inline comment") #Inline comment
+
+# print my name
+print("GeorgetheKing")
+'''
+Script Usage:
+To run comment.py, use the command "python3 comments.py" at the command line.
+
+Output:
+The output of comments.py are miscellaneous statements used as arguments to demonstrate the print () function 
+'''
+
+'''
+# Prints numbers 1-20
+for i in range(1,21):
+	print(i)
+'''
+
+# Prints the names of all my pets	
+pet_names = ["Stella", "Blueberry", "Sunny", "Bird", "Zoscia", "New Pet"]
+for pet in pet_names:
+	print(pet)
+    
